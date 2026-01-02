10f672c27ad47cb82124b2823ae88ef47007adbd
PtrCz <79607371+PtrCz@users.noreply.github.com>
2026-01-02 20:04:24 +0100

    docs: add documentation to the string.hpp file

--- include/uni-cpp/string.hpp
+++ include/uni-cpp/string.hpp
@@ -1,6 +1,11 @@
 #ifndef UNI_CPP_STRING_HPP
 #define UNI_CPP_STRING_HPP
 
+/// @file
+///
+/// @brief Provides ASCII- and UTF-encoded string types.
+///
+
 #include "uchar.hpp"
 #include <string>
 
@@ -13,23 +18,53 @@
 
 namespace upp
 {
+    /// @brief Concept that checks whether `Allocator` is an allocator type for `ValueType` type.
+    ///
+    /// @tparam ValueType The value type the allocator must allocate.
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<typename Allocator, typename ValueType>
     concept allocator_for = requires(Allocator& alloc) {
         typename Allocator::value_type;
         alloc.deallocate(alloc.allocate(1uz), 1uz);
     } && std::is_same_v<typename Allocator::value_type, ValueType>;
 
+    /// @brief ASCII string type.
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<allocator_for<char> Allocator = std::allocator<char>>
-    class basic_ascii_string;
+    class basic_ascii_string
+    {
+    };
 
+    /// @brief UTF-8 string type.
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<allocator_for<char8_t> Allocator = std::allocator<char8_t>>
-    class basic_utf8_string;
+    class basic_utf8_string
+    {
+    };
 
+    /// @brief UTF-16 string type.
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<allocator_for<char16_t> Allocator = std::allocator<char16_t>>
-    class basic_utf16_string;
+    class basic_utf16_string
+    {
+    };
 
+    /// @brief UTF-32 string type.
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<allocator_for<char32_t> Allocator = std::allocator<char32_t>>
-    class basic_utf32_string;
+    class basic_utf32_string
+    {
+    };
 
     namespace impl
     {
@@ -64,9 +99,27 @@ namespace upp
         };
     } // namespace impl
 
+    /// @brief Concept for identifying UTF-encoded string types defined by the uni-cpp library.
+    ///
+    /// This concept checks whether the type `T` is a template instantiation of
+    /// `basic_utf8_string`, `basic_utf16_string`, or `basic_utf32_string`.
+    ///
+    /// @see basic_utf8_string, basic_utf16_string, basic_utf32_string
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<typename T>
     concept unicode_string_type = impl::is_unicode_string<T>::value;
 
+    /// @brief Concept for identifying string types defined by the uni-cpp library.
+    ///
+    /// This concept checks whether the type `T` is a template instantiation of
+    /// `basic_ascii_string`, `basic_utf8_string`, `basic_utf16_string`, or `basic_utf32_string`.
+    ///
+    /// @see basic_ascii_string, basic_utf8_string, basic_utf16_string, basic_utf32_string
+    ///
+    /// @headerfile "" <uni-cpp/string.hpp>
+    ///
     template<typename T>
     concept string_type = unicode_string_type<T> || impl::is_ascii_string<T>::value;
 

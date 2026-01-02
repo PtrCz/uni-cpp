b4dc7c797448f7b697bb8affc2cb1794fe42812c
PtrCz <79607371+PtrCz@users.noreply.github.com>
2026-01-02 15:47:33 +0100

    refactor: implement any_of utility using concepts

--- include/uni-cpp/impl/utility.hpp
+++ include/uni-cpp/impl/utility.hpp
@@ -1,12 +1,12 @@
 #ifndef UNI_CPP_IMPL_UTILITY_HPP
 #define UNI_CPP_IMPL_UTILITY_HPP
 
-#include <type_traits>
+#include <concepts>
 
 namespace upp::impl
 {
     template<typename T, typename... Ts>
-    constexpr bool is_any_of = (std::is_same_v<T, Ts> || ...);
+    concept any_of = (std::same_as<T, Ts> || ...);
 } // namespace upp::impl
 
 #endif // UNI_CPP_IMPL_UTILITY_HPP

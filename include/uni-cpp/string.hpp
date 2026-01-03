4f56b291f8a356861a04f8fbdd5e4f4b78219ae3
PtrCz <79607371+PtrCz@users.noreply.github.com>
2026-01-03 23:14:45 +0100

    refactor: implement string types by deriving from string base classes

--- include/uni-cpp/string.hpp
+++ include/uni-cpp/string.hpp
@@ -16,6 +16,8 @@
 #include <memory>
 #include <memory_resource>
 
+// DOXYGEN-PREPROCESSOR: PREPROCESSOR_START
+
 namespace upp
 {
     /// @brief Concept that checks whether `Allocator` is an allocator type for `ValueType` type.
@@ -35,36 +37,28 @@ namespace upp
     /// @headerfile "" <uni-cpp/string.hpp>
     ///
     template<allocator_for<char> Allocator = std::allocator<char>>
-    class basic_ascii_string
-    {
-    };
+    class basic_ascii_string;
 
     /// @brief UTF-8 string type.
     ///
     /// @headerfile "" <uni-cpp/string.hpp>
     ///
     template<allocator_for<char8_t> Allocator = std::allocator<char8_t>>
-    class basic_utf8_string
-    {
-    };
+    class basic_utf8_string;
 
     /// @brief UTF-16 string type.
     ///
     /// @headerfile "" <uni-cpp/string.hpp>
     ///
     template<allocator_for<char16_t> Allocator = std::allocator<char16_t>>
-    class basic_utf16_string
-    {
-    };
+    class basic_utf16_string;
 
     /// @brief UTF-32 string type.
     ///
     /// @headerfile "" <uni-cpp/string.hpp>
     ///
     template<allocator_for<char32_t> Allocator = std::allocator<char32_t>>
-    class basic_utf32_string
-    {
-    };
+    class basic_utf32_string;
 
     namespace impl
     {
@@ -134,12 +128,12 @@ namespace upp
         };
 
         template<string_encoding>
-        struct encoding_properties
+        struct encoding_traits
         {
         };
 
         template<>
-        struct encoding_properties<string_encoding::ascii>
+        struct encoding_traits<string_encoding::ascii>
         {
             using char_type      = ascii_char;
             using code_unit_type = char;
@@ -148,7 +142,7 @@ namespace upp
         };
 
         template<>
-        struct encoding_properties<string_encoding::utf8>
+        struct encoding_traits<string_encoding::utf8>
         {
             using char_type      = uchar;
             using code_unit_type = char8_t;
@@ -157,7 +151,7 @@ namespace upp
         };
 
         template<>
-        struct encoding_properties<string_encoding::utf16>
+        struct encoding_traits<string_encoding::utf16>
         {
             using char_type      = uchar;
             using code_unit_type = char16_t;
@@ -166,7 +160,7 @@ namespace upp
         };
 
         template<>
-        struct encoding_properties<string_encoding::utf32>
+        struct encoding_traits<string_encoding::utf32>
         {
             using char_type      = uchar;
             using code_unit_type = char32_t;
@@ -177,12 +171,16 @@ namespace upp
         template<string_encoding Encoding, typename Allocator>
         class basic_string_base
         {
+        private:
+            static constexpr auto s_encoding = Encoding;
+            using traits                     = encoding_traits<s_encoding>;
+
         public:
-            using code_unit_type  = encoding_properties<Encoding>::code_unit_type;
+            using code_unit_type  = traits::code_unit_type;
             using allocator_type  = Allocator;
             using size_type       = std::allocator_traits<Allocator>::size_type;
             using difference_type = std::allocator_traits<Allocator>::difference_type;
-            using char_type       = encoding_properties<Encoding>::char_type;
+            using char_type       = traits::char_type;
 
         public:
         protected:
@@ -195,6 +193,9 @@ namespace upp
         private:
             using base = basic_string_base<Encoding, Allocator>;
 
+            static constexpr auto s_encoding = Encoding;
+            using traits                     = encoding_traits<s_encoding>;
+
         protected:
             using base::m_string;
 
@@ -202,6 +203,79 @@ namespace upp
             using base::base;
         };
     } // namespace impl
+
+    template<allocator_for<char> Allocator>
+    class basic_ascii_string : public impl::basic_string_base<impl::string_encoding::ascii, Allocator> // DOXYGEN-PREPROCESSOR: HIDE_INHERITANCE
+    {
+    private:
+        static constexpr auto s_encoding = impl::string_encoding::ascii;
+
+        using traits = impl::encoding_traits<s_encoding>;
+        using base   = impl::basic_string_base<s_encoding, Allocator>;
+
+    public:
+        using base::base;
+
+    private:
+        using base::m_string;
+    };
+
+    template<allocator_for<char8_t> Allocator>
+    class basic_utf8_string : public impl::basic_utf_string_base<impl::string_encoding::utf8, Allocator> // DOXYGEN-PREPROCESSOR: HIDE_INHERITANCE
+    {
+    private:
+        static constexpr auto s_encoding = impl::string_encoding::utf8;
+
+        using traits = impl::encoding_traits<s_encoding>;
+        using base   = impl::basic_utf_string_base<s_encoding, Allocator>;
+
+    public:
+        using base::base;
+
+    private:
+        using base::m_string;
+    };
+
+    template<allocator_for<char16_t> Allocator>
+    class basic_utf16_string : public impl::basic_utf_string_base<impl::string_encoding::utf16, Allocator> // DOXYGEN-PREPROCESSOR: HIDE_INHERITANCE
+    {
+    private:
+        static constexpr auto s_encoding = impl::string_encoding::utf16;
+
+        using traits = impl::encoding_traits<s_encoding>;
+        using base   = impl::basic_utf_string_base<s_encoding, Allocator>;
+
+    public:
+        using base::base;
+
+    private:
+        using base::m_string;
+    };
+
+    template<allocator_for<char32_t> Allocator>
+    class basic_utf32_string : public impl::basic_utf_string_base<impl::string_encoding::utf32, Allocator> // DOXYGEN-PREPROCESSOR: HIDE_INHERITANCE
+    {
+    private:
+        static constexpr auto s_encoding = impl::string_encoding::utf32;
+
+        using traits = impl::encoding_traits<s_encoding>;
+        using base   = impl::basic_utf_string_base<s_encoding, Allocator>;
+
+    public:
+        using base::base;
+
+    private:
+        using base::m_string;
+    };
+
+    template<allocator_for<char8_t> Allocator = std::allocator<char8_t>>
+    using basic_ustring = basic_utf8_string<Allocator>;
+
+    using ascii_string = basic_ascii_string<>;
+    using utf8_string  = basic_utf8_string<>;
+    using utf16_string = basic_utf16_string<>;
+    using utf32_string = basic_utf32_string<>;
+    using ustring      = basic_ustring<>;
 } // namespace upp
 
 #endif // UNI_CPP_STRING_HPP

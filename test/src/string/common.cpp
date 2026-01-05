5368769e960db37c6bb647822e88fe7b4521ad6e
PtrCz <79607371+PtrCz@users.noreply.github.com>
2026-01-05 15:24:45 +0100

    refactor: add a utility run_for_each_string_type for testing

--- test/src/string/common.cpp
+++ test/src/string/common.cpp
@@ -5,30 +5,29 @@
 #include <type_traits>
 #include <concepts>
 
+template<typename Callable>
+void run_for_each_string_type(const Callable& callable)
+{
+    callable.template operator()<upp::ascii_string>();
+    callable.template operator()<upp::utf8_string>();
+    callable.template operator()<upp::utf16_string>();
+    callable.template operator()<upp::utf32_string>();
+}
+
 TEST_CASE("string type traits", "[string types]")
 {
-    auto check_type_traits_for = [&]<typename StringType>() {
+    run_for_each_string_type([&]<typename StringType>() {
         STATIC_CHECK(std::default_initializable<StringType>);
         STATIC_CHECK(std::destructible<StringType>);
         STATIC_CHECK(std::is_nothrow_move_constructible_v<StringType>);
         STATIC_CHECK(std::is_nothrow_move_assignable_v<StringType>);
-    };
-
-    check_type_traits_for.template operator()<upp::ascii_string>();
-    check_type_traits_for.template operator()<upp::utf8_string>();
-    check_type_traits_for.template operator()<upp::utf16_string>();
-    check_type_traits_for.template operator()<upp::utf32_string>();
+    });
 }
 
 TEST_CASE("string default constructor", "[string types]")
 {
-    auto check_default_constructor_for = [&]<typename StringType>() {
+    run_for_each_string_type([&]<typename StringType>() {
         StringType empty;
         StringType with_allocator{std::allocator<typename StringType::code_unit_type>()};
-    };
-
-    check_default_constructor_for.template operator()<upp::ascii_string>();
-    check_default_constructor_for.template operator()<upp::utf8_string>();
-    check_default_constructor_for.template operator()<upp::utf16_string>();
-    check_default_constructor_for.template operator()<upp::utf32_string>();
+    });
 }

bf119b98b13c5142f2258d243f43bdbb9b8f481e
PtrCz <79607371+PtrCz@users.noreply.github.com>
2026-05-22 21:32:41 +0200

    feat(UDG): implement `list` and `clean` commands

--- dev/tools/unicode_data_generator/src/unicode_data_generator/dispatcher/dispatcher.py
+++ dev/tools/unicode_data_generator/src/unicode_data_generator/dispatcher/dispatcher.py
@@ -1,9 +1,10 @@
 from . import context
 
+from ..core import datasets
+
 class CommandDispatcher:
     def __init__(self, global_context: context.GlobalContext):
         self.global_context = global_context
-        print(f'Global: {self.global_context}')
 
     def generate(self, context: context.GenerateContext):
         print(f'Generate: {context}')
@@ -12,7 +13,43 @@ class CommandDispatcher:
         print(f'Analyze: {context}')
 
     def clean(self, context: context.CleanContext):
-        print(f'Clean: {context}')
+        def delete_dir(name: str, path):
+            print(f'[*] Removing the {name} directory: {path}')
+
+            import shutil
+
+            try:
+                shutil.rmtree(path)
+            except OSError:
+                print(f'[-] Failed to remove the {name} directory')
+
+        def get_dir(path, unicode_version: str | None):
+            if unicode_version is None:
+                return path
+            else:
+                return path / unicode_version
+
+        cache_dir = get_dir(self.global_context.cache_dir, context.unicode_version)
+        output_dir = get_dir(self.global_context.output_dir, context.unicode_version)
+
+        match context.target:
+            case 'all':
+                delete_dir('cache', cache_dir)
+                delete_dir('output', output_dir)
+            
+            case 'cache':
+                delete_dir('cache', cache_dir)
+
+            case 'output':
+                delete_dir('output', output_dir)
+
 
     def list(self, context: context.ListContext):
-        print(f'List: {context}')
+        match context.target:
+            case 'datasets':
+                for dataset in datasets.datasets():
+                    print(dataset)
+
+            case 'tests':
+                for test in datasets.test_datasets():
+                    print(test)

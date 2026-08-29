4bee9e368aac47124c9b483b1a781b6dd2af2c45
PtrCz <79607371+PtrCz@users.noreply.github.com>
2026-08-29 16:36:29 +0200

    test: test normalize_view on single-pass ranges

--- test/src/ranges/normalize_view.cpp
+++ test/src/ranges/normalize_view.cpp
@@ -12,38 +12,44 @@
 
 TEST_CASE("normalize_view", "[ranges][normalization]")
 {
+    auto test_equality = [](auto&& actual_range, auto&& expected_range) {
+        CHECK(upp_test::ranges::equal(actual_range, expected_range));
+
+        if constexpr (std::ranges::bidirectional_range<decltype(actual_range)>)
+            CHECK(upp_test::ranges::equal(actual_range | upp::views::reverse, expected_range | std::views::reverse));
+    };
+
     const auto& test_data = upp_test::load_normalization_test_data();
 
     const auto decode = upp::views::mark_as_valid_utf32 | upp::views::decode_valid_utf32;
 
     for (const auto& test_case : test_data)
     {
-        const auto source   = test_case.source | decode;
         const auto test_nfc = test_case.nfc | decode, test_nfd = test_case.nfd | decode;
         const auto test_nfkc = test_case.nfkc | decode, test_nfkd = test_case.nfkd | decode;
 
-        auto test_equality = [](auto&& actual_range, auto&& expected_range) {
-            CHECK(upp_test::ranges::equal(actual_range, expected_range));
-            CHECK(upp_test::ranges::equal(actual_range | upp::views::reverse, expected_range | std::views::reverse));
+        auto test_for_range = [&](auto&& range) {
+            test_equality(range | upp::views::normalize_to_nfc, test_nfc);
+            test_equality(test_nfc | upp::views::normalize_to_nfc, test_nfc), test_equality(test_nfd | upp::views::normalize_to_nfc, test_nfc);
+
+            test_equality(test_nfkc | upp::views::normalize_to_nfc, test_nfkc), test_equality(test_nfkd | upp::views::normalize_to_nfc, test_nfkc);
+
+            test_equality(range | upp::views::normalize_to_nfd, test_nfd);
+            test_equality(test_nfd | upp::views::normalize_to_nfd, test_nfd), test_equality(test_nfc | upp::views::normalize_to_nfd, test_nfd);
+
+            test_equality(test_nfkd | upp::views::normalize_to_nfd, test_nfkd), test_equality(test_nfkc | upp::views::normalize_to_nfd, test_nfkd);
+
+            test_equality(range | upp::views::normalize_to_nfkc, test_nfkc);
+            test_equality(test_nfc | upp::views::normalize_to_nfkc, test_nfkc), test_equality(test_nfd | upp::views::normalize_to_nfkc, test_nfkc);
+            test_equality(test_nfkc | upp::views::normalize_to_nfkc, test_nfkc), test_equality(test_nfkd | upp::views::normalize_to_nfkc, test_nfkc);
+
+            test_equality(range | upp::views::normalize_to_nfkd, test_nfkd);
+            test_equality(test_nfc | upp::views::normalize_to_nfkd, test_nfkd), test_equality(test_nfd | upp::views::normalize_to_nfkd, test_nfkd);
+            test_equality(test_nfkc | upp::views::normalize_to_nfkd, test_nfkd), test_equality(test_nfkd | upp::views::normalize_to_nfkd, test_nfkd);
         };
 
-        test_equality(source | upp::views::normalize_to_nfc, test_nfc);
-        test_equality(test_nfc | upp::views::normalize_to_nfc, test_nfc), test_equality(test_nfd | upp::views::normalize_to_nfc, test_nfc);
-
-        test_equality(test_nfkc | upp::views::normalize_to_nfc, test_nfkc), test_equality(test_nfkd | upp::views::normalize_to_nfc, test_nfkc);
-
-        test_equality(source | upp::views::normalize_to_nfd, test_nfd);
-        test_equality(test_nfd | upp::views::normalize_to_nfd, test_nfd), test_equality(test_nfc | upp::views::normalize_to_nfd, test_nfd);
-
-        test_equality(test_nfkd | upp::views::normalize_to_nfd, test_nfkd), test_equality(test_nfkc | upp::views::normalize_to_nfd, test_nfkd);
-
-        test_equality(source | upp::views::normalize_to_nfkc, test_nfkc);
-        test_equality(test_nfc | upp::views::normalize_to_nfkc, test_nfkc), test_equality(test_nfd | upp::views::normalize_to_nfkc, test_nfkc);
-        test_equality(test_nfkc | upp::views::normalize_to_nfkc, test_nfkc), test_equality(test_nfkd | upp::views::normalize_to_nfkc, test_nfkc);
-
-        test_equality(source | upp::views::normalize_to_nfkd, test_nfkd);
-        test_equality(test_nfc | upp::views::normalize_to_nfkd, test_nfkd), test_equality(test_nfd | upp::views::normalize_to_nfkd, test_nfkd);
-        test_equality(test_nfkc | upp::views::normalize_to_nfkd, test_nfkd), test_equality(test_nfkd | upp::views::normalize_to_nfkd, test_nfkd);
+        test_for_range(test_case.source | decode);
+        test_for_range(test_case.source | upp_test::views::to_input | decode);
     }
 }
 
